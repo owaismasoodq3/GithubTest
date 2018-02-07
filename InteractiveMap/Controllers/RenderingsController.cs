e0b7b9aa9829b453e8b5f44e3dcbe8ee115128c1
Owais <omasood@q3tech.com>
2018-02-07 13:35:06 +0530

    Removed the static line

--- InteractiveMap/Controllers/RenderingsController.cs
+++ InteractiveMap/Controllers/RenderingsController.cs
@@ -11,7 +11,7 @@ namespace InteractiveMap.Controllers
         // GET: Renderings
         public ActionResult Index()
         {
-            string test = "this is to test git";
+            string test = "";
             return View("Map");
         }
     }
